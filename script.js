479e80cfefdb1deea474ff50b0dab2490d694a4c
krish koli <krishkoli536@gmail.com>
2026-04-18 19:41:39 +0530

    horizontal scroll text effect added.

--- script.js
+++ script.js
@@ -378,3 +378,38 @@ window.addEventListener("load", () => {
     }
   });
 });
+
+document.fonts.ready.then(() => {
+    const wrapper = document.querySelector(".Horizontal");
+    const text = document.querySelector(".Horizontal__text");
+
+    if (wrapper && text) {
+      const split10 = SplitText.create(text, { type: "chars,words" });
+
+      const scrollTween = gsap.to(text, {
+        xPercent: -100,
+        ease: "none",
+        scrollTrigger: {
+          trigger: wrapper,
+          pin: true,
+          end: "+=5000px",
+          scrub: true
+        }
+      });
+
+      split10.chars.forEach((char) => {
+        gsap.from(char, {
+          yPercent: "random(-200,200)",
+          rotation: "random(-20,20)",
+          ease: "back.out(1.2)",
+          scrollTrigger: {
+            trigger: char,
+            containerAnimation: scrollTween,
+            start: "left 100%",
+            end: "left 30%",
+            scrub: 1
+          }
+        });
+      });
+    }
+  });
